a59fd1fb4ecbeb0a26cd54bcb50c772ef442bb19
Ignacio <joseignacio.perezdelgado@iessantaana.com>
2020-10-23 13:33:22 +0100

    Ejercicio al 70%

--- src/main/java/dad/javafx/iniciosesion/Controller.java
+++ src/main/java/dad/javafx/iniciosesion/Controller.java
@@ -1,5 +1,43 @@
 package dad.javafx.iniciosesion;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
+public class Controller {
+	private View view = new View();
+	private Model model = new Model();
+
+	public Controller() {
+
+		// model.nombreProperty().bind(view.getNombre().textProperty());
+		view.getUsuarioText().textProperty().bind(model.usuarioProperty());
+		view.getContraseñaPassword().textProperty().bind(model.contraseñaProperty());
+
+		view.getAccederButton().setOnAction(e -> onAccederAction(e));
+
+		// view.getCancelarButton().setOnAction(e -> onNombreAction(e));
+	}
+	
+	private void alertaDenegado() {
+		Alert alerta = new Alert(AlertType.ERROR);
+		alerta.setTitle("Iniciar sesión");
+		alerta.setHeaderText("Acceso denegado");
+		alerta.setContentText("El usuario y/o contraseña no son validos.");
+
+		alerta.showAndWait();
+	}
+
+	private void alertaPermitido() {
+		Alert alerta = new Alert(AlertType.INFORMATION);
+		alerta.setTitle("Iniciar sesión");
+		alerta.setHeaderText("Acceso permitido");
+		alerta.setContentText("Las credenciales de acceso son válidas");
+
+		alerta.showAndWait();
+	}
+	
+	private void onAccederAction(ActionEvent e) {
+		// TODO Auto-generated method stub
+	}
 }
--- src/main/java/dad/javafx/iniciosesion/View.java
+++ src/main/java/dad/javafx/iniciosesion/View.java
@@ -1,10 +1,12 @@
 package dad.javafx.iniciosesion;
 
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
